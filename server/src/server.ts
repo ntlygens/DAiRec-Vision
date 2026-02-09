b8848f2debe0d0b336e350ffc8fe1d9664170395
ntlygens <ntlygens@gmail.com>
2026-02-09 17:03:33 -0500

    server from server folder update
    Signed-off-by: ntlygens <ntlygens@gmail.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,6 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { connectToDatabase } from "./database.ts";
-// import { employeeRouter } from "./employee.routes.ts";
 import { uiRouter } from "./ui-interface.routes.ts";
 import { ssiRouter } from "./ss-interface.routes.ts";
 
@@ -36,23 +35,3 @@ await connectToDatabase(MONGODB_URI)
         console.error("Failed to connect to the database.", error);
         process.exit(1);
     });
-
-
-// export async function initializeServer(app: express.Express) {
-//     // const app = express()
-//     app.use(cors());
-//     console.log("MONGODB_URI:", MONGODB_URI);
-//     await connectToDatabase(MONGODB_URI as string)
-//     // await connectToDatabase(`${MONGODB_URI}`)
-
-//     .then(() => {
-//         app.listen(5200, () => {
-//             console.log("Server is running on port 5200");
-//         });
-//         console.log("Database connection established");
-//     })
-//     .catch((error) => {
-//         console.error("Failed to connect to the database.", error);
-//         process.exit(1);
-//     });
-
